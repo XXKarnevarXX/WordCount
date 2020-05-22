1d25b50f621e567157b0987433f62377b9ba55b4
XXKarnevalXXX <1617529498@qq.com>
2020-05-22 14:48:28 +0800

    单词计数

---
+++ xx.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include<malloc.h>
+int main(int argc, char const *argv[])
+{
+
+    FILE *fp;
+    int line = 1, str = 0, word = 0, pu = 0, ch = 0;
+    char *name = "";
+	char *str2 = "-c";
+	char *str3 = "-w";
+	char *path = "./";
+	char path2[20] = "";
+	strcpy(path2,argv[2]);
+	
+	name = (char*)malloc(strlen(path) + strlen(path2));
+	strcpy(name,path);
+	strcat(name,path2);
+
+    if ((fp = fopen(name, "r")) == NULL)
+    {
+        printf("can't open file");
+        return 0;
+    }
+  
+
+    ch = fgetc(fp);
+    while (ch != EOF)
+    {
+    
+        if (ch == ' ' || ch == ',')
+        {
+            word++;
+            
+        }
+        str++;
+        ch = fgetc(fp);
+    }
+    word++;
+
+    if(*(argv[1]+1) == 'c')
+    {
+        printf("×Ö·ûÊý£º%d",str);
+    }
+    else{
+    	
+    	printf("µ¥´ÊÊý£º%d",word);
+	}
+    fclose(fp);
+  
+    return 0;
+}
